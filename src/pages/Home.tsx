ec916223155fcf330a3460443d9c34931c25b70b
othmanordaski <rouissatothman@gmail.com>
2024-07-03 21:07:43 +0100

    add components to the home page

---
+++ src/pages/Home.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import Header from "../components/Header";
+import NavBar from "../components/NavBar";
+import Footer from "../components/Footer";
+import Copyright from "../components/Copyright";
+import HeroSection from "../components/HeroSection";
+import NewsDashboard from "../components/Actualites";
+import NewsLetter from "../components/NewsLetter";
+
+const Home: React.FC = () => {
+  return (
+    <>
+      <Header />
+      <NavBar />
+      <HeroSection />
+      <NewsDashboard />
+      <NewsLetter />
+      <Footer />
+      <Copyright />
+    </>
+  );
+};
+
+export default Home;
