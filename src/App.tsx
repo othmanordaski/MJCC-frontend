a4929d2aed674fed9f4bae3a8a53e9e17d958fed
othmanordaski <rouissatothman@gmail.com>
2024-07-06 00:40:45 +0100

    add the login page and all the dependecies needed

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
+import Login from "./components/Login";
 const App: React.FC = () => {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </Router>
   );
