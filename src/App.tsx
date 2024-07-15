5a341bff5e52f5153b3d6da38f6f758576da584c
othmanordaski <rouissatothman@gmail.com>
2024-07-15 20:26:37 +0100

    Update App component structure and routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
-import Login from "./components/Login";
+import SignUp from "./components/SignUp";
 const App: React.FC = () => {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
+        <Route path="/auth/signup" element={<SignUp />} />
       </Routes>
     </Router>
   );
