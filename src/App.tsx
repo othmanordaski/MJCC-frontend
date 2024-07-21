dd7478b32df9fcbd99f7a1d55dee331768c30562
othmanordaski <rouissatothman@gmail.com>
2024-07-21 23:42:17 +0100

    feat: enhance app with react-query, react-hot-toast, and email verification
    - Add QueryClientProvider to manage query caching
    - Include Toaster for toast notifications
    - Implement email verification route

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,24 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { queryClient } from "./lib/react-query";
+import { QueryClientProvider } from "react-query";
+
+import { Toaster } from "react-hot-toast";
 import Home from "./pages/Home";
 import SignUp from "./components/SignUp";
+import EmailVerification from "./components/EmailVerification";
 const App: React.FC = () => {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/auth/signup" element={<SignUp />} />
-      </Routes>
-    </Router>
+    <QueryClientProvider client={queryClient}>
+      <Router>
+        <Toaster position="top-center" reverseOrder={false} />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/auth/signup" element={<SignUp />} />
+          <Route path="/verify-email" element={<EmailVerification />} />
+        </Routes>
+      </Router>
+    </QueryClientProvider>
   );
 };
 
