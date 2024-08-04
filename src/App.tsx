9551d12d87be704a0fc9d5ae99e5cfe218fc15c6
othmanordaski <rouissatothman@gmail.com>
2024-08-04 17:01:51 +0100

    feat: Add AuthProvider for authentication context management

--- src/App.tsx
+++ src/App.tsx
@@ -2,23 +2,26 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { queryClient } from "./lib/react-query";
 import { QueryClientProvider } from "react-query";
-
 import { Toaster } from "react-hot-toast";
 import Home from "./pages/Home";
 import SignUp from "./components/SignUp";
 import EmailVerification from "./components/EmailVerification";
+import { AuthProvider } from "./context/AuthContext";
+
 const App: React.FC = () => {
   return (
-    <QueryClientProvider client={queryClient}>
-      <Router>
-        <Toaster position="top-center" reverseOrder={false} />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/auth/signup" element={<SignUp />} />
-          <Route path="/verify-email" element={<EmailVerification />} />
-        </Routes>
-      </Router>
-    </QueryClientProvider>
+    <AuthProvider>
+      <QueryClientProvider client={queryClient}>
+        <Router>
+          <Toaster position="top-center" reverseOrder={false} />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/auth/signup" element={<SignUp />} />
+            <Route path="/verify-email" element={<EmailVerification />} />
+          </Routes>
+        </Router>
+      </QueryClientProvider>
+    </AuthProvider>
   );
 };
 
