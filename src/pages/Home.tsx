c7d28d7228475c3810e00a46f3b0e75db90dd786
othmanordaski <rouissatothman@gmail.com>
2024-08-26 23:18:39 +0100

    Remove conditional login modal from Home component for simplified state management and cleaner UI.

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -6,14 +6,10 @@ import Copyright from "../components/Copyright";
 import HeroSection from "../components/HeroSection";
 import NewsDashboard from "../components/Actualites";
 import NewsLetter from "../components/NewsLetter";
-import { useAuthContext } from "../context/AuthContext";
-import Login from "../components/Login";
 
 const Home: React.FC = () => {
-  const { isModalOpen } = useAuthContext();
   return (
     <>
-      {isModalOpen && <Login />}
       <HeroSection />
       <NewsDashboard />
       <NewsLetter />
