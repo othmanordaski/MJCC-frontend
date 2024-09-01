1f59f8f6486f8ba0ff52bcfa9251aeb05da94eef
othmanordaski <rouissatothman@gmail.com>
2024-09-01 17:51:03 +0100

    refactor: update and improve sign-up form design
    - Refactored code for better readability and maintainability.
    - Updated the design of the sign-up form for a more modern and user-friendly interface.

--- src/components/SignUp.tsx
+++ src/components/SignUp.tsx
@@ -7,7 +7,6 @@ import { useAuth } from "../hooks/useAuth";
 import CircularProgress from "@mui/material/CircularProgress";
 import { FcGoogle } from "react-icons/fc";
 import { FaFacebook } from "react-icons/fa";
-import bgimage from "../assets/SignUp-image-background/bg-image.jpg";
 import { useAuthContext } from "../context/AuthContext";
 import { toast } from "react-toastify";
 
@@ -110,133 +109,121 @@ const SignUp: React.FC = () => {
   };
 
   return (
-    <div className="flex justify-center items-center w-full min-h-screen bg-gray-200 p-4">
-      <div className="w-full max-w-4xl bg-white shadow-lg rounded-lg overflow-hidden">
-        <div className="grid grid-cols-1 md:grid-cols-2">
-          <div className="hidden md:block ">
-            <img
-              className="w-full h-full object-cover"
-              src={bgimage}
-              alt="bg image signup"
-            />
-          </div>
+    <div className="flex justify-center items-center w-full min-h-screen bg-white p-4">
+      <div className="p-8">
+        <form onSubmit={handleSubmit(onSubmit)} className="space-y-6 w-[400px]">
+          <img
+            className="h-14 w-auto"
+            src="https://mjcc.gov.ma/wp-content/uploads/2021/12/mjcc_black.svg"
+            alt="MJCC Logo"
+          />
 
-          <div className="p-8">
-            <form onSubmit={handleSubmit(onSubmit)} className="space-y-6">
-              <img
-                className="h-14 w-auto"
-                src="https://mjcc.gov.ma/wp-content/uploads/2021/12/mjcc_black.svg"
-                alt="MJCC Logo"
-              />
-
-              <div className="space-y-4">
-                <div>
-                  <label
-                    htmlFor="username"
-                    className="block text-sm font-medium text-gray-700"
-                  >
-                    Username
-                  </label>
-                  <input
-                    {...register("username")}
-                    type="text"
-                    placeholder="Your username"
-                    className="mt-1 block w-full px-3 py-2 bg-white border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-orange-500 focus:border-orange-500"
-                  />
-                  {errors.username && (
-                    <p className="mt-1 text-sm text-red-600">
-                      {errors.username.message}
-                    </p>
-                  )}
-                </div>
-
-                <div>
-                  <label
-                    htmlFor="email"
-                    className="block text-sm font-medium text-gray-700"
-                  >
-                    Email
-                  </label>
-                  <input
-                    {...register("email")}
-                    type="email"
-                    placeholder="Your email"
-                    className="mt-1 block w-full px-3 py-2 bg-white border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-orange-500 focus:border-orange-500"
-                  />
-                  {errors.email && (
-                    <p className="mt-1 text-sm text-red-600">
-                      {errors.email.message}
-                    </p>
-                  )}
-                </div>
-
-                <div>
-                  <label
-                    htmlFor="password"
-                    className="block text-sm font-medium text-gray-700"
-                  >
-                    Password
-                  </label>
-                  <input
-                    {...register("password")}
-                    type="password"
-                    placeholder="Your password"
-                    className="mt-1 block w-full px-3 py-2 bg-white border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-orange-500 focus:border-orange-500"
-                  />
-                  {errors.password && (
-                    <p className="mt-1 text-sm text-red-600">
-                      {errors.password.message}
-                    </p>
-                  )}
-                </div>
-              </div>
-
-              <button
-                className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-orange-600 hover:bg-orange-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-orange-500"
-                type="submit"
+          <div className="space-y-4">
+            <div>
+              <label
+                htmlFor="username"
+                className="block text-sm font-medium text-gray-700"
               >
-                {isSubmitting ? (
-                  <CircularProgress sx={{ color: "white" }} size={24} />
-                ) : (
-                  "S'inscrire"
-                )}
-              </button>
-
-              <div className="flex justify-center items-center ">
-                <p className="text-sm text-gray-600 ">
-                  Already have an account?
-                  <button
-                    type="button"
-                    className="font-medium ml-1 text-orange-600 hover:text-orange-500 hover:underline"
-                    onClick={handleLoginClick}
-                  >
-                    Se connecter
-                  </button>
+                Username
+              </label>
+              <input
+                {...register("username")}
+                type="text"
+                placeholder="Your username"
+                className="mt-1 block w-full px-3 py-2 bg-white border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-orange-500 focus:border-orange-500"
+              />
+              {errors.username && (
+                <p className="mt-1 text-sm text-red-600">
+                  {errors.username.message}
                 </p>
-              </div>
-              <div>
-                <div className="mt-6 grid grid-cols-2 gap-3">
-                  <button
-                    type="button"
-                    className="w-full inline-flex justify-center items-center py-2 px-4 border border-gray-300 rounded-md shadow-sm bg-white text-sm font-medium text-gray-500 hover:bg-gray-200"
-                    onClick={handleGoogleSignup}
-                  >
-                    <FcGoogle className="text-2xl mr-2" />
-                    <span>Google</span>
-                  </button>
-                  <button
-                    type="button"
-                    className="w-full inline-flex justify-center items-center py-2 px-4 border border-gray-300 rounded-md shadow-sm bg-white text-sm font-medium text-gray-500 hover:bg-gray-200"
-                    onClick={handleFacebookSignup}
-                  >
-                    <FaFacebook className="text-2xl mr-2 text-blue-600" />
-                    <span>Facebook</span>
-                  </button>
-                </div>
-              </div>
-            </form>
+              )}
+            </div>
+
+            <div>
+              <label
+                htmlFor="email"
+                className="block text-sm font-medium text-gray-700"
+              >
+                Email
+              </label>
+              <input
+                {...register("email")}
+                type="email"
+                placeholder="Your email"
+                className="mt-1 block w-full px-3 py-2 bg-white border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-orange-500 focus:border-orange-500"
+              />
+              {errors.email && (
+                <p className="mt-1 text-sm text-red-600">
+                  {errors.email.message}
+                </p>
+              )}
+            </div>
+
+            <div>
+              <label
+                htmlFor="password"
+                className="block text-sm font-medium text-gray-700"
+              >
+                Password
+              </label>
+              <input
+                {...register("password")}
+                type="password"
+                placeholder="Your password"
+                className="mt-1 block w-full px-3 py-2 bg-white border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-orange-500 focus:border-orange-500"
+              />
+              {errors.password && (
+                <p className="mt-1 text-sm text-red-600">
+                  {errors.password.message}
+                </p>
+              )}
+            </div>
           </div>
-        </div>
+
+          <button
+            className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-orange-600 hover:bg-orange-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-orange-500"
+            type="submit"
+          >
+            {isSubmitting ? (
+              <CircularProgress sx={{ color: "white" }} size={24} />
+            ) : (
+              "S'inscrire"
+            )}
+          </button>
+
+          <div className="flex justify-center items-center ">
+            <p className="text-sm text-gray-600 ">
+              Already have an account?
+              <button
+                type="button"
+                className="font-medium ml-1 text-orange-600 hover:text-orange-500 hover:underline"
+                onClick={handleLoginClick}
+              >
+                Se connecter
+              </button>
+            </p>
+          </div>
+          <div>
+            <div className="mt-6 grid grid-cols-2 gap-3">
+              <button
+                type="button"
+                className="w-full inline-flex justify-center items-center py-2 px-4 border border-gray-300 rounded-md shadow-sm bg-white text-sm font-medium text-gray-500 hover:bg-gray-200"
+                onClick={handleGoogleSignup}
+              >
+                <FcGoogle className="text-2xl mr-2" />
+                <span>Google</span>
+              </button>
+              <button
+                type="button"
+                className="w-full inline-flex justify-center items-center py-2 px-4 border border-gray-300 rounded-md shadow-sm bg-white text-sm font-medium text-gray-500 hover:bg-gray-200"
+                onClick={handleFacebookSignup}
+              >
+                <FaFacebook className="text-2xl mr-2 text-blue-600" />
+                <span>Facebook</span>
+              </button>
+            </div>
+          </div>
+        </form>
       </div>
     </div>
   );
