1330b664e883d0f6cb166ea210923361d788eebd
othmanordaski <rouissatothman@gmail.com>
2024-09-03 19:23:51 +0100

    Create basic SearchPage component and integrate it into routing

---
+++ src/pages/SearchPage.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { useParams } from "react-router-dom";
+
+const SearchPage: React.FC = () => {
+  return (
+    <div>
+      <h1>Search Page</h1>
+    </div>
+  );
+};
+
+export default SearchPage;
